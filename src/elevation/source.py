2a90c218acb90051a7940bef4471bbdf0cd355a9
thomas-god <thomas.godlewski@icloud.com>
2025-07-29 08:39:45 +0200

    chore: log response error when downloading elevation files

--- src/elevation/source.py
+++ src/elevation/source.py
@@ -42,23 +42,31 @@ def get_files(files: List[str]):
         if not os.path.isfile(os.path.join(config["ASSETS"], file))
     ]
 
-    if len(missing_files) > 0:
-        print(f"{len(missing_files)} elevation files need to be downloaded")
-        with requests.Session() as session:
-            session.auth = (config["LOGIN"], config["PWD"])
+    if len(missing_files) == 0:
+        return
 
-            for file in files:
-                filename = file.removesuffix(".hgts")
-                url = f"{config['URL_PREFIX']}/NASADEM_SHHP_{filename}/NASADEM_SHHP_{filename}.zip"
-                r1 = session.request("get", url)
+    print(f"{len(missing_files)} elevation files need to be downloaded")
 
-                r = session.get(r1.url, auth=(config["LOGIN"], config["PWD"]))
+    with requests.Session() as session:
+        session.auth = (config["LOGIN"], config["PWD"])
 
-                if r.ok:
-                    z = zipfile.ZipFile(io.BytesIO(r.content))
-                    with TemporaryDirectory() as tmpdir:
-                        z.extractall(tmpdir)
-                        Path(os.path.join(tmpdir, file)).rename(
-                            os.path.join(config["ASSETS"], file)
-                        )
-                        print(f"Downloaded elevation file {file}")
+        for file in files:
+            filename = file.removesuffix(".hgts")
+            url = f"{config['URL_PREFIX']}/NASADEM_SHHP_{filename}/NASADEM_SHHP_{filename}.zip"
+            r1 = session.request("get", url)
+
+            r = session.get(r1.url, auth=(config["LOGIN"], config["PWD"]))
+
+            if r.ok:
+                z = zipfile.ZipFile(io.BytesIO(r.content))
+                with TemporaryDirectory() as tmpdir:
+                    z.extractall(tmpdir)
+                    Path(os.path.join(tmpdir, file)).rename(
+                        os.path.join(config["ASSETS"], file)
+                    )
+                    print(f"Downloaded elevation file {file}")
+
+            else:
+                print(
+                    f"Error when trying to download {file}: response has {r.status_code}"
+                )
